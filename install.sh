e9d5d0fa11b0293caceedaf063b88963388f0f2b
N-coder82 <redacted/private>
2024-06-15 15:29:12 -0700

    changed installer to use vars

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # ====== Script Info ======
 #title          :install.sh
-#description    :Installs ghuser.
+#description    :Installs $progname.
 #author         :N-coder82
 #date           :5/18/24
 #version        :1.0
@@ -11,21 +11,21 @@
 #bash_version   :3.2.57(1)-release
 # =========================
 #!/bin/bash
-
+progname="ghuser"
 error_exit() {
     local RED='\033[0;31m'
     local NC='\033[0m' # No Color
     echo -e "${RED}An error occured:${NC} Please try again."
-    rm -rf "$TMPDIR/ghuser"
+    rm -rf "$TMPDIR/$progname"
     exit 1
 }
 cd "$TMPDIR" || error_exit
-echo "Downloading ghuser..."
-git clone https://github.com/N-coder82/ghuser.git ghuser
-cd "$TMPDIR/ghuser" || error_exit
-curl -O https://raw.githubusercontent.com/N-coder82/ghuser/main/ghuser.sh || error_exit
-chmod +x ghuser.sh
-sudo mv ghuser.sh /usr/local/bin/ghuser || error_exit
+echo "Downloading $progname..."
+git clone https://github.com/N-coder82/$progname.git $progname
+cd "$TMPDIR/$progname" || error_exit
+curl -O https://raw.githubusercontent.com/N-coder82/$progname/main/$progname.sh || error_exit
+chmod +x $progname.sh
+sudo mv $progname.sh /usr/local/bin/$progname || error_exit
 cd ..
-rm -rf "$TMPDIR/ghuser" || error_exit
+rm -rf "$TMPDIR/$progname" || error_exit
 echo Done!
